7e8e0ab6f5cd29877be843b43e4a2630137f9ed9
maccam912 <maccam912@gmail.com>
2021-08-05 20:59:06 -0500

    some more css

--- lib/tradiex_platform_web/components/nav_component.ex
+++ lib/tradiex_platform_web/components/nav_component.ex
@@ -3,20 +3,22 @@ defmodule TradiexPlatformWeb.NavComponent do
 
   def classes(assigns, name) do
     if String.contains?(to_string(assigns.view), name) do
-      "tab tab-active"
+      "btn btn-primary btn-sm rounded-btn navbar-center"
     else
-      "tab"
+      "btn btn-ghost btn-sm rounded-btn"
     end
   end
 
   def render(assigns) do
     ~L"""
-    <nav class="tabs tabs-boxed">
+    <nav class="navbar">
+    <div class="navbar-start"></div>
     <%= live_redirect("Account", to: Routes.account_path(@socket, :index), class: classes(@socket, "Account")) %>
     <%= live_redirect("Portfolio", to: Routes.portfolio_path(@socket, :index), class: classes(@socket, "Portfolio")) %>
     <%= live_redirect("Orders", to: Routes.orders_path(@socket, :index), class: classes(@socket, "Orders")) %>
     <%= live_redirect("Trade", to: Routes.trade_path(@socket, :index), class: classes(@socket, "Trade")) %>
     <%= live_redirect("Options Chain", to: Routes.options_chain_path(@socket, :index), class: classes(@socket, "OptionsChain")) %>
+    <div class="navbar-end"></div>
     </nav>
     """
   end
